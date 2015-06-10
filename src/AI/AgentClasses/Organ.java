1fd2b86a435116962ae5ea5eac3925873da43dc8
MatthieuGallet <matthieugallet@Matthieu-G.local>
2015-06-10 22:46:48 +0800

    correct commit 2nd try

---
+++ src/AI/AgentClasses/Organ.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AI.AgentClasses;
+
+import AI.AgentClasses.Senses.See;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author jonathan
+ */
+public class Organ {
+    private String organName;
+    private ArrayList<Action> actions;
+    private ArrayList<Sense> sensors;
+    private HashMap<String, Integer> characteristics;
+    
+    
+    public Organ(String n,Action a){
+        organName=n;
+        actions=new ArrayList();
+        sensors=new ArrayList();
+        characteristics= new HashMap<String, Integer>();
+        actions.add(a);
+    }
+    
+    public Organ(String n,Sense s){
+        organName=n;
+        actions=new ArrayList();
+        sensors=new ArrayList();
+        characteristics= new HashMap<String, Integer>();
+        sensors.add(s);
+    }
+        
+    public Organ(String n){
+        organName=n;
+        actions=new ArrayList();
+        sensors=new ArrayList();
+        characteristics= new HashMap<String, Integer>();
+    };
+  
+    public String getOrganName(){
+        return organName;
+    }
+    public void setOrganName(String organName) {
+        this.organName = organName;
+    }
+
+    public ArrayList<Action> getActions() {
+        return actions;
+    }
+    public void addAction(Action a) {
+        this.actions.add(a);
+    }
+    public ArrayList<Sense> getSenses() {
+        return sensors;
+    }
+    public void addSens(Sense a) {
+        this.sensors.add(a);
+    }
+    
+    public void addCharacteristic(String name, int value){
+        characteristics.put(name, value);
+    }
+    
+    public int getCharacteristic(String name){
+        return characteristics.containsKey(name)? characteristics.get(name) : -1;
+    }
+    
+    public void setCharacteristic(String name, int value){
+        /* it the characteristic already exists, it modify it.
+         * else it adds a new one with the value.
+        */
+        if (characteristics.containsKey(name)) {
+            characteristics.replace(name, characteristics.get(name), value);
+        } else {
+            addCharacteristic(name, value);
+        }     
+    }
+    
+    public Organ copy() throws CloneNotSupportedException{
+        Organ neworgan= new Organ(this.getOrganName());
+        neworgan.actions=new ArrayList();
+        for(Action a: this.getActions()){
+            neworgan.addAction(a.copy());
+        }
+        neworgan.sensors=new ArrayList();
+        for(Sense s: this.getSenses()){
+            neworgan.addSens(s.copy());
+        }
+        neworgan.characteristics= (HashMap<String, Integer>) this.characteristics.clone();
+        
+        return neworgan;
+    }
+
+}
---
+++ src/AI/AgentClasses/Senses/See.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AI.AgentClasses.Senses;
+
+import AI.AgentClasses.Sense;
+import AI.AgentClasses.Agent;
+import AI.Main;
+import AI.Element;
+import AI.NonElement;
+import AI.Square;
+import AI.World;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Halyna
+ */
+public class See extends Sense{
+
+    int optimalLight = 50;
+    public See(int range) {
+        super(range, 0); // See can only SEE elements, not detect variables
+    }
+
+    @Override
+    public String getName() {
+        return "see";
+    }
+
+    @Override
+    public int senseVariable(World world, int agentX, int agentY, int squareX, int squareY, String name) {
+        // seeing cannot detect a variable
+        return 0;
+    }
+
+    @Override
+    public Element senseElement(World world, int agentX, int agentY, int squareX, int squareY) {
+        int slopeX = squareX - agentX;
+        int slopeY = squareY - agentY;
+        int div = GCD(slopeX, slopeY);
+        slopeX /= div;
+        slopeY /= div; // get the smallest increment possible for the vector
+        boolean obstacle = false;
+        
+        // lets check inbetween the two points without considering the frontier
+        int x = agentX + slopeX;
+        int y = agentY + slopeY;
+        while (x != squareX && y != squareY) {
+            try {
+                if (world.getElement(x, y) != null) // there is an obstacle 
+                {
+                    obstacle = true;
+                    break;
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(See.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            x += slopeX;
+            y += slopeY;
+        }
+        if (!obstacle)
+            try {
+                int light = world.getVariable("light", squareX, squareY);
+                float random = world.getRandom();
+                float quotient = light/optimalLight;
+                if (quotient < random) {
+                    return new NonElement();
+                }                
+                return world.getElement(squareX,squareY); // no need to check through the frontiere
+            } catch (Exception ex) {
+                Logger.getLogger(See.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        // now lets check if the agent could see through the frontiere (if an obstacle was found in the direct path)
+        x = squareX + slopeX;
+        y = squareY + slopeY;
+        int width = world.getSize()[0];
+        int height = world.getSize()[1];
+        while ( (x >=0 && y >= 0) && ( x < width && y < height) ) { // while x and y are inside the square
+            try {
+                if (world.getElement(x, y) != null) // there is an obstacle 
+                {
+                    obstacle = true;
+                    break;
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(See.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            x = squareX + slopeX;
+            y = squareY + slopeY;
+        
+        }
+        // lets check the other way
+        x = agentX - slopeX;
+        y = agentY - slopeY;
+        while ( (x >=0 && y >= 0) && ( x < width && y < height) ) { // while x and y are inside the square
+            try {
+                if (world.getElement(x, y) != null) // there is an obstacle 
+                {
+                    obstacle = true;
+                    break;
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(See.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            x = agentX - slopeX;
+            y = agentY - slopeY;
+        
+        }
+        if (obstacle)
+            return new NonElement(); // we couldnt see anything because of obstacles
+        else
+            try {
+                int light = world.getVariable("light", squareX, squareY);
+                float random = world.getRandom();
+                float quotient = light/optimalLight;
+                if (quotient < random) {
+                   return new NonElement();
+                }                
+                return world.getElement(squareX, squareY); // no obstacles
+            } 
+            catch (Exception ex) {
+                Logger.getLogger(See.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        return new NonElement();
+    }
+    
+    private int GCD(int a, int b) {
+        if (b==0) return a;
+        return GCD(b,a%b);
+    }
+
+    
+}
